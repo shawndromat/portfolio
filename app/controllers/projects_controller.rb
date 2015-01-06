bcfebb260f06b1da84641045c850584f50577350
Shawna <shawna@appacademy.io>
2015-01-05 19:39:54 -0800

    styling, add project to view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.visible
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,12 @@ class Project < ActiveRecord::Base
   def self.visible
     self.all.where(visible: true)
   end
+
+  def github=(path)
+    self.github_url = "http://www.github.com/shawndromat/#{path}"
+  end
+
+  def short_name
+    self.name.downcase.gsub(' ', '-')
+  end
 end
